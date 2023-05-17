1a31441dbc9c7af8e32d1412fcfbcfdd00e4eefb
Faakhir Rehman <faakhir.rehman123@gmail.com>
2023-05-17 16:26:07 +0500

    Angular Routing Added

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-navbar',
@@ -12,22 +14,23 @@ export class NavbarComponent implements OnInit {
   model: any = {};
   
 
-  constructor(public accountSerive:AccountService) { }
+  constructor(public accountSerive: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login(){
     this.accountSerive.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
+      next: () => {
+        this.router.navigateByUrl('/members')
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
     })
   }
 
   logout(){
     this.accountSerive.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
